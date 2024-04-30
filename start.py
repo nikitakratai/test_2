02120776bc9ccff3a62a6d4f8f9efd8ff1e14a8c
nikitakratai <159303435+nikitakratai@users.noreply.github.com>
2024-04-30 18:59:43 +0700

    Update start.py

--- start.py
+++ start.py
@@ -18,3 +18,12 @@ matrix = [[a, b, c, d],[e, f, g, h], [m, n, l, k]]
 sentence = ['Mary', 'had', 'a', 'little', 'lamb', '...' ,'ohh', 'no....', 'there is no more this lamb =))', 'because it was eaten xD']
 for i in range(len(sentence)):
   print(i, sentence[i])
+
+
+for n in range(2, 10):
+  for x in range(2, n):
+    if n % x == 0:
+      print(n, 'equals', x, '*', n//x)
+      break
+else:
+  print(n, 'is a prime number')
