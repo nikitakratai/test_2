5618ffe6007bc61017ceb23d63d71710626823d2
nikitakratai <159303435+nikitakratai@users.noreply.github.com>
2024-05-09 09:02:15 +0700

    Create logic.py

---
+++ logic.py
@@ -0,0 +1,7 @@
+integer = int(input("Please enter an integer: "))
+if integer == 120:
+  print('One hundred and twenty')
+elif (integer > 1) and (integer < 120):
+  print('in range 1..120')
+else:
+  print('More than 120')
