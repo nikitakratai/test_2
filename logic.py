0e93f35770255fb264a59e971c94e51c7cff53d4
nikitakratai <159303435+nikitakratai@users.noreply.github.com>
2024-05-17 07:48:51 +0700

    Update logic.py

--- logic.py
+++ logic.py
@@ -5,3 +5,11 @@ elif (integer > 1) and (integer < 120):
   print('in range 1..120')
 else:
   print('More than 120')
+
+
+def my_function1():
+"""1 This function does nothing.
+2 But call my_function1.__doc__ 
+3 will return these 3 lines..
+"""
+pass
